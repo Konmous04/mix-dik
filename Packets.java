9c89dc687ede57298583dc64cc1d75a8cdffff98
Konstantinos Mousoulis <konmous04@gmail.com>
2025-11-25 12:50:40 +0200

    First Commit

---
+++ Packets.java
@@ -0,0 +1,30 @@
+public class Packets {
+    private final String name;
+    private final int id;
+    private final int arrivalTime;
+    private final int sourceStationId;
+
+    public Packets(String name, int id, int arrivalTime, int sourceStationId){
+        this.name = name;
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.sourceStationId = sourceStationId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getArrivalTime(){
+        return arrivalTime;
+    }
+
+    public int getSourceStationId(){
+        return sourceStationId;
+    }
+
+}
